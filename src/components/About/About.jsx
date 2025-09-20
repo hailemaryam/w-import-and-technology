b61e3cd6ca322bff7fd58a872b33ce4060028c90
Kaleb Menbere <kalebmenbere@gmail.com>
2025-09-20 11:42:33 +0300

    some edits

---
+++ src/components/About/About.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./About.css"; // Optional CSS file for styling
+
+function About() {
+  return (
+    <section className="about">
+      <img src="/images/exact_embed.svg" alt="Character" />
+      <div className="about-text">
+        <p>ABOUT KIDOPIA ----</p>
+        <h2
+          data-en="EXPERIENCE NEXT-LEVEL ENTERTAINMENT WITH KIDOPIA – YOUR ULTIMATE PREMIUM HTML5 GAMING PORTAL."
+          data-am="የቀጣይ ደረጃ መዝናኛን ከKIDOPIA ጋር ይለማመዱ - የእርስዎ የመጨረሻው ፕሪሚየም HTML5 ጨዋታ መግቢያ።"
+        >
+          EXPERIENCE NEXT-LEVEL ENTERTAINMENT WITH KIDOPIA – YOUR ULTIMATE PREMIUM HTML5 GAMING PORTAL.
+        </h2>
+        <p
+          data-en="Play hundreds of high-quality games instantly — no downloads, no waiting, just pure fun. Fast, sleek, and built for any device, KIDOPIA turns every moment into a gaming adventure."
+          data-am="በመቶዎች የሚቆጠሩ ከፍተኛ ጥራት ያላቸውን ጨዋታዎችን በቅጽበት ይጫወቱ - ምንም ማውረድ፣ መጠበቅ የለም፣ ንጹህ አዝናኝ ብቻ። ፈጣን፣ ቄንጠኛ እና ለማንኛውም መሳሪያ የተሰራ KIDOPIA እያንዳንዱን አፍታ ወደ የጨዋታ ጀብዱነት ይቀየራል።"
+        >
+          Play hundreds of high-quality games instantly — no downloads, no waiting, just pure fun. Fast, sleek, and built for any device, KIDOPIA turns every moment into a gaming&nbsp;adventure.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
--- src/components/Kidopia.jsx
+++ src/components/Kidopia.jsx
@@ -2,7 +2,7 @@ import "./Kidopia.css";
 import Header from "./Header/Header";
 import Hero from "./Hero/Hero"; 
 import Box from "./Box/Box"; 
-
+import About from "./About/About";
 
 function Kidopia() {
 
@@ -11,8 +11,12 @@ function Kidopia() {
 <div className="fullscreen">
    {/* Header */}
       <Header />
+      <main>
       <Hero />
       <Box />
+     
+      </main>
+
 
 </div>
   )
